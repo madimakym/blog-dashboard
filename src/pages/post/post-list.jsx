ac0ff41e18c31a4a033395ff35bd9425fdf73077
makymadi <makymadi@gmail.com>
2022-03-13 15:28:26 +0000

    update

---
+++ src/pages/post/post-list.jsx
@@ -0,0 +1,14 @@
+import { Breadcrumb } from 'antd';
+
+function PostList() {
+    return (
+        <>
+            <Breadcrumb>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>Posts</Breadcrumb.Item>
+            </Breadcrumb>
+            Post list
+        </>
+    );
+}
+export default PostList;
